b1bd57eeb28810814750032540b053a638bc102f
syen9904 <syen9904@gmail.com>
2024-05-09 03:58:16 -0400

    graph precompute

--- src/network_generator.py
+++ src/network_generator.py
@@ -1,16 +1,10 @@
 import os
 import sys
-import streamlit as st
 import networkx as nx
 from pyvis.network import Network
-import streamlit.components.v1 as components
-import logging
 
-sys.path.append(os.getcwd() + '/src')
+sys.path.append(os.getcwd())
 from functions import getCodes
-from network_generator import generate_html
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s') 
 
 def find_node(line, keys, false_keys, comment_keys):
     for start_key in keys:
@@ -79,7 +73,6 @@ def parse(codes, file):
                 cte[input_key] = True
         for input_key in cte:
             result['input'].pop(input_key)
-
     return result
 
 def getEdges(omop_tables):
@@ -101,33 +94,13 @@ def getEdges(omop_tables):
 
     return edges
 
-def create_graph(omop_tables):
-    logging.info("Graph is not created")
+def generate_html(): 
+    omop_tables = ['person', 'observation_period', 'visit_occurrence', 'visit_detail', 'condition_occurrence', 'drug_exposure', 'procedure_occurrence', 'device_exposure', 'measurement', 'observation', 'death', 'note', 'note_nlp', 'specimen', 'fact_relationship', 'location', 'care_site', 'provider', 'payer_plan_period', 'cost', 'drug_era', 'dose_era', 'condition_era', 'episode', 'episode_event', 'metadata', 'cdm_source', 'concept', 'vocabulary', 'domain', 'concept_class', 'concept_relationship', 'relationship', 'concept_synonym', 'concept_ancestor', 'source_to_concept_map', 'drug_strength']
+
     G = nx.DiGraph()
     edges = getEdges(omop_tables)
     for a, b in edges:
         G.add_edge(a, b)
     net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white")
     net.from_nx(G)
-    net_html = net.generate_html()
-    return net_html
-
-omop_tables = ['person', 'observation_period', 'visit_occurrence', 'visit_detail', 'condition_occurrence', 'drug_exposure', 'procedure_occurrence', 'device_exposure', 'measurement', 'observation', 'death', 'note', 'note_nlp', 'specimen', 'fact_relationship', 'location', 'care_site', 'provider', 'payer_plan_period', 'cost', 'drug_era', 'dose_era', 'condition_era', 'episode', 'episode_event', 'metadata', 'cdm_source', 'concept', 'vocabulary', 'domain', 'concept_class', 'concept_relationship', 'relationship', 'concept_synonym', 'concept_ancestor', 'source_to_concept_map', 'drug_strength']
-
-st.title('Knowledge Graph')
-
-#if 'html_content' not in st.session_state:
-#    st.session_state.html_content.create_graph(omop_tables)
-#st.write(st.session_state.html_content)
-
-net_path = os.getcwd() + '/src/graph.html'
-if os.path.exists(net_path) == False:
-    logging.info("Graph has not been created")
-    generate_html()
-    logging.info("Graph created")
-
-with open (net_path, 'r') as net:
-    st.write(f"Number of edges from PMAP to OMOP", 299)
-    st.write("Is this graph a DAG: ", False)
-    html_content = net.read()
-    components.html(html_content, height=800)
+    net.save_graph('src/graph.html')
