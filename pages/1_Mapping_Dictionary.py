a8827020a5f77a4086ac2a8a85ea3dadad64a644
syen9904 <syen9904@gmail.com>
2024-05-12 02:17:37 -0400

    column display

--- pages/1_Mapping_Dictionary.py
+++ pages/1_Mapping_Dictionary.py
@@ -82,28 +82,51 @@ def search(search_str, columns, table_name):
     finally:
         if conn:
             conn.close()
-            print("Database connection is closed")
+            logging.info("Database connection is closed")
 
 def getDf(user_query, columns, table_name):
-    return pd.DataFrame(search(user_query, columns, table_name), columns=columns)
+    df = pd.DataFrame(search(user_query, columns, table_name), columns=columns).set_index("source_code")
+    df['target_concept_id'] = st.session_state.athena_prefix + df['target_concept_id'].apply(str)
+    return df
+
+st.set_page_config(
+    layout="wide",
+)
 
 if 'columns' not in st.session_state: 
     st.session_state.parent_dir = os.path.dirname(os.getcwd())
     st.session_state.csv_path = st.session_state.parent_dir + "/cteVocabMapDF.csv"
     st.session_state.db_path = st.session_state.parent_dir + "/database.db"
     st.session_state.table_name = 'data'
-    st.session_state.COLUMNS = ['source_code', 'source_code_description', 'source_vocabulary_id', 'source_domain_id', 'source_concept_class_id', 'target_concept_id', 'target_concept_name', 'target_vocabulary_id', 'target_domain_id', 'target_concept_class_id']
+    st.session_state.COLUMNS = ['source_code', 'target_concept_id', 'source_code_description', 'target_concept_name', 'source_vocabulary_id', 'target_vocabulary_id', 'target_domain_id', 'target_concept_class_id']
     st.session_state.columns = create_db(csv_path=st.session_state.csv_path, db_path=st.session_state.db_path, table_name=st.session_state.table_name)
+    st.session_state.map = {"source_code": "Source Code", "target_concept_id": "Target Concept ID", "source_code_description": "Source Code Description", "target_concept_name": "Target Concept Name", "source_vocabulary_id": "Source Vocabulary ID", "target_vocabulary_id": "Target Vocabulary ID", "target_domain_id": "Target Domain ID", "target_concept_class_id": "Target Concept Class ID"}
+    st.session_state.default_str = "levophed"
+    st.session_state.user_query = st.session_state.default_str
+    st.session_state.athena_prefix = "https://athena.ohdsi.org/search-terms/terms/"
     if os.path.exists(st.session_state.csv_path): st.session_state.columns = st.session_state.COLUMNS
-    print('columns = ', st.session_state.columns)
+    st.session_state.column_config = {
+        "source_code": st.column_config.Column("Source Code", disabled=True), 
+        "target_concept_id": st.column_config.LinkColumn("Target Concept ID", width="small", disabled=True, display_text=st.session_state.athena_prefix + "([^&]*)"), 
+        "source_code_description": st.column_config.Column("Source Code Description", width="large", disabled=True), 
+        "target_concept_name": st.column_config.Column("Target Concept Name", width="large", disabled=True), 
+        "source_vocabulary_id": st.column_config.Column("Source Vocabulary ID", width="small", disabled=True), 
+        "target_vocabulary_id": st.column_config.Column("Target Vocabulary ID", width="small", disabled=True), 
+        "target_domain_id": st.column_config.Column("Target Domain ID", width="small", disabled=True), 
+        "target_concept_class_id": st.column_config.Column("Target Concept Class ID", width="small", disabled=True)        
+        }
+    logging.info(f"columns = {st.session_state.columns}")
 
 st.title('JHU ETL Mapping Dictionary')
-user_query = st.text_input("Enter your search string, it could be a description a concept code (ex: levophed)", "")
 
-if 'unfiltered_df' not in st.session_state: st.session_state.unfiltered_df = pd.DataFrame()
+user_query = st.text_input(f"Enter your search string, it could be a description a concept code (ex: {st.session_state.default_str})", value=st.session_state.default_str)
+
+if 'unfiltered_df' not in st.session_state: 
+    st.session_state.unfiltered_df = getDf(st.session_state.default_str, st.session_state.columns, st.session_state.table_name)
 
 if st.button('Search'):
-    st.session_state.unfiltered_df = getDf(user_query, st.session_state.columns, st.session_state.table_name)
+    st.session_state.user_query = user_query
+    st.session_state.unfiltered_df = getDf(st.session_state.user_query, st.session_state.columns, st.session_state.table_name)
 
 if 'filtered_df' not in st.session_state: st.session_state.filtered_df = pd.DataFrame()
 st.session_state.filtered_df = st.session_state.unfiltered_df
@@ -111,12 +134,12 @@ st.session_state.filtered_df = st.session_state.unfiltered_df
 st.sidebar.markdown("### Filters")
 filter_expander = {}
 filter_dict = {}
-columns_to_be_filtered = ['source_vocabulary_id', 'source_domain_id', 'source_concept_class_id', 'target_vocabulary_id', 'target_domain_id', 'target_concept_class_id']
+columns_to_be_filtered = ['source_vocabulary_id', 'target_vocabulary_id', 'target_domain_id', 'target_concept_class_id']
 
 for c in columns_to_be_filtered:
     no_filter = True
     if c not in st.session_state.unfiltered_df: continue
-    expander = st.sidebar.expander(c)
+    expander = st.sidebar.expander(st.session_state.map[c])
     filter_dict[c] = {}
     for t in st.session_state.unfiltered_df[c].unique():
         filter_dict[c][t] = expander.checkbox(f"{t} ({(st.session_state.unfiltered_df[c]==t).sum()})", False, c+','+t)
@@ -127,7 +150,9 @@ for c in columns_to_be_filtered:
             if not filter_dict[c][t]:
                 st.session_state.filtered_df = st.session_state.filtered_df[st.session_state.filtered_df[c] != t]        
 
-st.write(f"{len(st.session_state.unfiltered_df)} results found for '{user_query}'")
+if user_query != '':
+    st.write(f'{len(st.session_state.unfiltered_df)} results found for "{st.session_state.user_query}".')
+    st.write("If your source code is not found in the results, it is highly possible that the data element with that source code is not being mapped to OMOP. Please contact JH OMOP team.")
 
 if len(st.session_state.filtered_df) > 0:
-    st.write(st.session_state.filtered_df)
+    st.data_editor(st.session_state.filtered_df, use_container_width=True, column_config=st.session_state.column_config)
